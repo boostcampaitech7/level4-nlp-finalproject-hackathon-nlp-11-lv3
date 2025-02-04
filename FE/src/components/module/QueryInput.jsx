675629f08b7389514645c8f1dd79a2008f848caa
Jeongmi Park <qkrwjdal419@gmail.com>
2025-02-04 21:50:23 +0900

    feat: add module

---
+++ FE/src/components/module/QueryInput.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
+import Button from '@mui/material/Button';
+import CustomSpace from '../atom/CustomBox'
+import Input from '../atom/input'
+
+export default function QueryInput() {
+    const navigate = useNavigate();
+  
+    function goToChat() {
+      navigate('/chat');
+    }
+
+    return (
+        <CustomSpace>
+            <Input placeholder='질문을 입력하세요.' />
+            <Button onClick={goToChat}>검색</Button>
+        </CustomSpace>
+    )
+}

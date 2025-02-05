bed2e0771ce03a5557e9f12bad000eb0155b917c
Jeongmi Park <qkrwjdal419@gmail.com>
2025-02-05 11:56:49 +0900

    feat: update IndexWidget

--- FE/src/components/atom/CustomContainer.jsx
+++ FE/src/components/atom/CustomContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { styled, Box } from '@mui/system'
 
 const CustomBox = styled(Box)(
-    ({ color, radius, width, height, my }) => `
+    ({ color, radius, width, height, flexDirection, my }) => `
     background-color: #${color};
     color: #ffffff;
     font-family: Pretendard-Regular;
@@ -10,6 +10,7 @@ const CustomBox = styled(Box)(
     height: ${height}px;
     border-radius: ${radius}px;
     display: flex;
+    flex-direction: ${flexDirection};
     justify-content: center;
     alight-items: center;
     margin: ${my}% 0 0 0;
@@ -17,8 +18,8 @@ const CustomBox = styled(Box)(
     `
 )
 
-export default function CustomContainer({ children, type, placeholder, onChange, color, radius, width, height, my }) {
+export default function CustomContainer({ children, type, placeholder, onChange, color, radius, width, height, flexDirection, my }) {
     return (
-        <CustomBox color={color} radius={radius} width={width} height={height} my={my} type={type} placeholder={placeholder} onChange={onChange}>{children}</CustomBox>
+        <CustomBox color={color} radius={radius} width={width} height={height} flexDirection={flexDirection} my={my} type={type} placeholder={placeholder} onChange={onChange}>{children}</CustomBox>
     )
 }
--- FE/src/components/module/IndexWidget.jsx
+++ FE/src/components/module/IndexWidget.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import moment from 'moment'
 
@@ -10,40 +10,66 @@ const URL = 'https://apis.data.go.kr/1160100/service/GetMarketIndexInfoService/g
 const apiKey = import.meta.env.VITE_Index_API_KEY;
 const currentDate = moment().format('YYYYMMDD');
 
-function getIndexSuccess(res) {
-  const data = res.data.response.body.items.item[0];
-  const basDt = data.basDt; // 날짜
-  const clpr = data.clpr; // 종가
-  const hipr = data.hipr; // 고가
-  const lopr = data.lopr; // 저가
-  const vs = data.vs; // 전일대비
-  const fltRt = data.fltRt;
-
-  // console.log(basDt, clpr, hipr, lopr, vs, fltRt);
-}
-
-function getIndex() {
-  axios
-    .get(
-      URL,
-      {
-        params: {
-          serviceKey: apiKey,
-          resultType: 'json',
-          baseDt: currentDate,
-          idxNm: "코스피"
-        }
-      }
-    )
-    .then(getIndexSuccess);
-}
-
 export default function IndexWidget() {
-    return (
-      <Box onClick={getIndex} sx={{ display: 'flex', width: '85%', cursor: 'pointer' }}>
-        <CustomContainer color='39393B' radius='8' width='100' height='250' my='90'>
-          <CustomText size='xs'>지수</CustomText>
+  const [indexData, setIndexData] = useState();
+
+  function getIndexSuccess(res) {
+    const data = res.data.response.body.items.item[0];
+
+    if (!data || data.length === 0) {
+      console.error('No data found in API response');
+      return;
+    }
+
+    setIndexData({
+      basDt: data.basDt, // 날짜
+      clpr: data.clpr, // 종가
+      hipr: data.hipr, // 고가
+      lopr: data.lopr, // 저가
+      vs: data.vs, // 전일 대비
+      fltRt: data.fltRt, // 등락률
+    });
+  }
+
+  function getIndex() {
+    axios
+      .get(
+        URL,
+        {
+          params: {
+            serviceKey: apiKey,
+            resultType: 'json',
+            baseDt: currentDate,
+            idxNm: '코스피'
+          }
+        }
+      )
+      .then(getIndexSuccess);
+  } 
+
+  useEffect(() => {
+    getIndex();
+  }, [])
+
+  return (
+    <Box sx={{ display: 'flex', width: '85%', cursor: 'pointer' }}>
+      {indexData ? (
+        <CustomContainer color='39393B' radius='8' width='100' height='250' flexDirection='column' my='90'>
+          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end' }}>
+            <CustomText size='s' weight='bold' mx='5'>코스피</CustomText>
+            <CustomText size='xs' color='second' my='1' mx='5'>{`${indexData.basDt.slice(4, 6)}.${indexData.basDt.slice(6)}`}</CustomText>
+          </Box>
+          <CustomText size='l' weight='bold'>{`${indexData.clpr}`}</CustomText>
+          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end' }}>
+            <CustomText size='s' color={parseFloat(indexData.vs) < 0 ? 'down' : 'up'} mx='5'>{`${indexData.vs}`}</CustomText>
+            <CustomText size='s' color={parseFloat(indexData.fltRt) < 0 ? 'down' : 'up'} mx='5'>{`${indexData.fltRt}%`}</CustomText>
+          </Box>
         </CustomContainer>
-      </Box>
-    )
+      ) : (
+        <CustomContainer color='39393B' radius='8' width='100' height='250' flexDirection='column' my='90'>
+          <CustomText size='xs'>Loading...</CustomText>
+        </CustomContainer>
+      )}
+    </Box>
+  );
 }

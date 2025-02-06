9cf1e69a432521e997588cb18de09b5ccea290f8
Jeongmi Park <qkrwjdal419@gmail.com>
2025-02-07 00:23:31 +0900

    feat: connect restful api

--- FE/src/api/api.jsx
+++ FE/src/api/api.jsx
@@ -1,8 +1,11 @@
 import axios from 'axios'
 
+const authorization = btoa('test@email.com:1234');
+
 const api = axios.create({
-    baseURL: 'localhost:8000/api',
+    baseURL: 'http://10.28.224.90:30685/api/',
     headers: {
+        'Authorization': `Basic ${authorization}`,
         'Content-Type': 'application/json',
     }
 });
--- FE/src/api/query.jsx
+++ FE/src/api/query.jsx
@@ -1,14 +1,14 @@
 import api from './api'
 
-function requestQuery(query, max_tokens, temperature) {
+function requestQuery(query, max_tokens, temperature, requestQuerySuccess, requestQueryFail) {
     api
-        .post('vi/query', {
+        .post('v1/query/', {
             query: query,
             max_tokens: max_tokens,
             temperature: temperature,
         })
-        .then(success)
-        .catch(fail);
+        .then(requestQuerySuccess)
+        .catch(requestQueryFail);
 }
 
 export { requestQuery };

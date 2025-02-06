af599ea90addef61eee8fff9e25161588233d6c7
Jeongmi Park <qkrwjdal419@gmail.com>
2025-02-06 17:05:09 +0900

    feat: add NewsWidget

--- FE/src/components/page/ChatPage.jsx
+++ FE/src/components/page/ChatPage.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 
-import Button from '@mui/material/Button';
-import { styled, Box } from '@mui/system'
-
+import { Box } from '@mui/system'
 import CustomText from '../atom/CustomText';
-import QueryOutput from '../module/QueryOutput';
-
 import SideBar from '../atom/SideBar'
+
 import IndexWidget from '../module/IndexWidget';
 import ExchangeRateWidget from '../module/ExchangeRateWidget';
-import NewsWidget from '../module/NewsWidget';
+import StockNewsWidget from '../module/StockNewsWidget';
+import QueryInput from '../module/QueryInput';
+import QueryOutput from '../module/QueryOutput';
 
 export default function MainPage() {
   function goToHome() {
@@ -20,19 +19,21 @@ export default function MainPage() {
 
   const location = useLocation();
   const query = location.state?.query;
+  const companyName = '네이버';
 
   return (
     <Box sx={{display: 'flex'}}>
       <SideBar>
         <IndexWidget />
         <ExchangeRateWidget />
-        <Box sx={{ marginTop: 'auto', marginBottom: '20px', width: '85%' }}>
-          <NewsWidget />
+        <Box sx={{ marginTop: 'auto', marginBottom: '20px', width: '80%' }}>
+          <StockNewsWidget>{companyName}</StockNewsWidget>
         </Box>
       </SideBar>
       
-      <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'flex-top', alignItems: 'center', width: '100vw'}}>
+      <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', width: '100vw', marginBottom: '40px'}}>
         <QueryOutput>{query}</QueryOutput>
+        <QueryInput height='95' />
       </Box>
     </Box>
   );

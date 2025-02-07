cdcc7ca6d916df51854306130887512205dd4cd8
Jeongmi Park <qkrwjdal419@gmail.com>
2025-02-07 19:49:39 +0900

    feat: apply daisyUI

--- FE/src/configs/theme.jsx
+++ FE/src/configs/theme.jsx
@@ -2,8 +2,8 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
     typography: {
-        fontFamily: "'GmarketSansMedium', sans-serif",
+        fontFamily: "'Pretendard-Regular', sans-serif",
     },
 });
 
-export default theme;
+export default theme; 

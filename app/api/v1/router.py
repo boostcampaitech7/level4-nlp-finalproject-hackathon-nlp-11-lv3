8d63b2cebc8c2823003f85a278ec9c2b3c1948ef
elwhyjay <lyj4032@naver.com>
2025-02-09 05:50:15 +0900

    feat: chatting endpoint 변경

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,9 +1,9 @@
-from api.v1.endpoints import chat, documents, query
+from api.v1.endpoints import documents, query, chatting
 from fastapi import APIRouter
 
 router = APIRouter()
 
 # 각 엔드포인트 라우터 등록
-router.include_router(chat.router, prefix="/chat", tags=["chat"])
+router.include_router(chatting.router, prefix="/chatting", tags=["chatting"])
 router.include_router(query.router, prefix="/query", tags=["query"])
 router.include_router(documents.router, prefix="/documents", tags=["documents"])

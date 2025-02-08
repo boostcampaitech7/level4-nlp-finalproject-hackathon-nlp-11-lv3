7857d0890704d4a5da2accb4a9c94cb10405987d
elwhyjay <lyj4032@naver.com>
2025-02-09 05:48:34 +0900

    path 버그수정
    *path 버그수정, 하드코딩된 path 추후 수정 예정

--- app/RAG/main.py
+++ app/RAG/main.py
@@ -8,8 +8,8 @@ from dotenv import load_dotenv
 from omegaconf import DictConfig
 
 # from source.finetune_ret import finetune
-from source.generate import generate
-from source.retrieve import retrieve
+#from source.generate import generate
+#from source.retrieve import retrieve
 from utils.vector_store import VectorStore
 
 
@@ -21,11 +21,11 @@ def main(cfg: DictConfig):
 
     if cfg.mode == "retrieve":
         print("test retrieval")
-        retrieve(cfg)
+        #retrieve(cfg)
 
     elif cfg.mode == "generate":
         print("test inference")
-        generate(cfg)
+        #generate(cfg)
 
     elif cfg.mode == "update_vectordb":
         print("벡터 DB 업데이트 시작")
@@ -40,8 +40,8 @@ def main(cfg: DictConfig):
             os.makedirs(old_data_dir)
 
         # JSON 파일 경로 설정
-        text_json_path = "../../PDF_OCR/new_data/text.json"
-        table_json_path = "../../PDF_OCR/new_data/table.json"
+        text_json_path = "../../PDF_OCR/new_data/All_data/text.json"
+        table_json_path = "../../PDF_OCR/new_data/All_data/table.json"
 
         # 파일이 존재하는지 확인
         if not (os.path.exists(text_json_path) and os.path.exists(table_json_path)):
@@ -51,10 +51,10 @@ def main(cfg: DictConfig):
         try:
             # 벡터 스토어 초기화 및 업데이트
             vector_store = VectorStore(cfg=cfg, persist_directory=vector_db_dir)
-            vector_store.update_company_vector_stores(text_json_path, table_json_path)
+            #vector_store.update_company_vector_stores(text_json_path, table_json_path)
             vector_store.update_all_vector_stores(text_json_path, table_json_path)
             # 처리된 파일 이동
-            vector_store.move_to_old_data([text_json_path, table_json_path], old_data_dir="../../PDF_OCR/old_data")
+            vector_store.move_to_old_data([text_json_path, table_json_path], old_data_dir="../../PDF_OCR/old_data",user_name="All_data")
             print("벡터 DB 업데이트 완료")
 
         except Exception as e:

47439fc13aa1a39874777ffd7dace3f47e45e500
Jeongmi Park <qkrwjdal419@gmail.com>
2025-02-09 23:46:07 +0900

    fix: ExchangeRateWidget logic

--- FE/src/components/module/ExchangeRateWidget.jsx
+++ FE/src/components/module/ExchangeRateWidget.jsx
@@ -15,59 +15,82 @@ export default function ExchangeRateWidget() {
   const [rateData, setRateData] = useState({});
   const [yesterdayRateData, setYesterdayRateData] = useState({});
   const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [todayLoading, setTodayLoading] = useState(true);
+  const [yesterdayloading, setYesterdayLoading] = useState(true);
 
-  async function fetchRates(isYesterday) {
-    const url = isYesterday ? HISTORICAL_URL : LIVE_URL;
-    const date = isYesterday ? moment().subtract(1, 'days').format('YYYY-MM-DD') : null;
-
+  async function fetchTodayRate() {
     try {
-      const res = await axios.get(url, {
+      const res = await axios.get(LIVE_URL, {
         params: {
           access_key: apiKey,
           currencies: 'KRW,JPY,EUR,CNY',
           source: 'USD',
-          ...(isYesterday && { date }),
-        },
-      });
+        }
+      })
 
       if (res.data.success) {
+        console.log('today: ', res.data);
         const usdToKrw = res.data.quotes.USDKRW;
         const jpyToKrw = res.data.quotes.USDJPY ? usdToKrw / res.data.quotes.USDJPY : null;
         const eurToKrw = res.data.quotes.USDEUR ? usdToKrw / res.data.quotes.USDEUR : null;
         const cnyToKrw = res.data.quotes.USDCNY ? usdToKrw / res.data.quotes.USDCNY : null;
 
-        if (isYesterday) {
-          setYesterdayRateData({
-            USD: usdToKrw,
-            JPY: jpyToKrw * 100,
-            EUR: eurToKrw,
-            CNY: cnyToKrw,
-          });
-        } else {
-          setRateData({
-            USD: usdToKrw,
-            JPY: jpyToKrw * 100,
-            EUR: eurToKrw,
-            CNY: cnyToKrw,
-          });
-        }
+        setRateData({
+          USD: usdToKrw,
+          JPY: jpyToKrw * 100,
+          EUR: eurToKrw,
+          CNY: cnyToKrw,
+        });
       } else {
         throw new Error(res.data.error.info);
       }
     } catch (err) {
       setError(err.message);
     } finally {
-      setLoading(false);
+      setTodayLoading(false);
+    }
+  }
+
+  async function fetchYesterdayRate() {
+    try {
+      const res = await axios.get(HISTORICAL_URL, {
+        params: {
+          access_key: apiKey,
+          currencies: 'KRW,JPY,EUR,CNY',
+          source: 'USD',
+          date: moment().subtract(1, 'days').format('YYYY-MM-DD'),
+        }
+      })
+
+      if (res.data.success) {
+        console.log('yesterday: ', res.data);
+        const usdToKrw = res.data.quotes.USDKRW;
+        const jpyToKrw = res.data.quotes.USDJPY ? usdToKrw / res.data.quotes.USDJPY : null;
+        const eurToKrw = res.data.quotes.USDEUR ? usdToKrw / res.data.quotes.USDEUR : null;
+        const cnyToKrw = res.data.quotes.USDCNY ? usdToKrw / res.data.quotes.USDCNY : null;
+
+        setYesterdayRateData({
+          USD: usdToKrw,
+          JPY: jpyToKrw * 100,
+          EUR: eurToKrw,
+          CNY: cnyToKrw,
+        });
+      } else {
+        throw new Error(res.data.error.info);
+      }
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setYesterdayLoading(false);
     }
   }
 
   useEffect(() => {
-    fetchRates(false); // 오늘 환율
-    fetchRates(true); // 어제 환율
+    fetchTodayRate();
+    fetchYesterdayRate();
   }, []);
 
-  if (error || loading) {
+  if (error) {
     return (
       <CustomContainer color='303030' radius='8' width='190px' height='210' my='20px'>
         <Box sx={{ display: 'flex', alignItems: 'center'}}>

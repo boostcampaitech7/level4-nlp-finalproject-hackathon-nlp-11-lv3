670cae6a6e0ba508d59bb92f015ddd34a09659ad
elwhyjay <lyj4032@naver.com>
2025-02-10 13:52:26 +0900

    feat: query,response log txt로 저장

--- app/api/v1/endpoints/query.py
+++ app/api/v1/endpoints/query.py
@@ -3,22 +3,25 @@ from fastapi import APIRouter, Depends, HTTPException
 from loguru import logger
 from schemas.rag import QueryRequest, QueryResponse
 from services.rag_service import RAGService
-
+import json
 router = APIRouter()
 rag_service = RAGService()
 
 
-@router.post("/", response_model=QueryResponse)
-async def query(request: QueryRequest, username: str = Depends(verify_credentials)):
+@router.post("", response_model=QueryResponse)
+async def query(request: QueryRequest): #, username: str = Depends(verify_credentials)):
     try:
-        logger.info(f"Received query request from {username}: {request.query}")
-
+        #logger.info(f"Received query request from {username}: {request.query}")
+        logger.info(f"Received query request: {request.query}")
         answer, retrieved_docs, processing_time, company = await rag_service.process_query(request)
 
         response = QueryResponse(
-            answer=answer, retrieved_documents=retrieved_docs, processing_time=processing_time, company=company
+            answer=answer, context=retrieved_docs, processing_time=processing_time, company=company
         )
-
+        logger.info(f"Query response: {response.answer}")
+        #log 에 저장
+        with open("result_log.txt", "a", encoding="utf-8") as f:
+            f.write(f"{request.query}\n{response}\n--------------------------------\n")
         logger.info(f"Query processed successfully in {processing_time:.2f} seconds")
         return response
 
